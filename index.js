0af994afa18683b8b70b140afdc8b798a10dc2b0
Zendal-dev <artzendal@gmail.com>
2021-01-11 13:38:33 +0200

    fix

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
+const http = require('http')
 
 const BOT_TOKEN = process.env.BOT_TOKEN
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 
 const bot = new Telegraf(BOT_TOKEN)
 
+http.createServer().listen(process.env.PORT || 5000).on('request', (req, res) => {
+   res.end('')
+})
+
 bot.start(ctx => ctx.reply('Enter city name'))
+
 bot.help((ctx) => ctx.reply('I can\'t help you'))
 
 bot.on('text', ctx => {
