020681ac482d6012619f636c9e95f06e1f094d5a
Zendal-dev <artzendal@gmail.com>
2021-01-11 14:17:32 +0200

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http')
 const BOT_TOKEN = process.env.BOT_TOKEN
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 
-const bot = new Telegraf(BOT_TOKEN)
+const bot = new Telegraf(BOT_TOKEN.trim())
 
 http.createServer().listen(process.env.PORT || 5000).on('request', (req, res) => {
    res.end('')
